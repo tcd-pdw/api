4d5c567307f19b7c7fc27ab9c4626b1915367dc2
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-06-25 16:16:51 -0300

    Create and Add method of register User

--- src/main/kotlin/moodmanager/api/controller/UserController.kt
+++ src/main/kotlin/moodmanager/api/controller/UserController.kt
@@ -1,21 +1,30 @@
 package moodmanager.api.controller
 
+import moodmanager.api.modelDTO.ResponseDTO
 import moodmanager.api.modelDTO.UserDTO
 import moodmanager.api.service.UserService
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
 
 @RestController
 @RequestMapping("/users")
-class UserController(private val userService: UserService){
+class UserController(private val userService: UserService) {
+
     @GetMapping
     fun getAllUsers():ArrayList<UserDTO> {
         return userService.getAllUsers()
     }
+//
+//    @GetMapping("/{username}")
+//    fun addUser(@PathVariable("id") username: String): String{
+//        val dataList = mapOf(Pair("Deu certo", 404))
+//        userService.addUser(UserDTO(0,username))
+//        return dataList.toString()
+//    }
 
-    @GetMapping("/{username}")
-    fun addUser(@PathVariable("id") username: String): String{
-        val dataList = mapOf(Pair("Deu certo", 404))
-        userService.addUser(UserDTO(0,username))
-        return dataList.toString()
+    @PostMapping("")
+    fun register(@RequestBody @Valid user: UserDTO): ResponseDTO {
+        return this.userService.register(user)
     }
 }
--- src/main/kotlin/moodmanager/api/service/UserService.kt
+++ src/main/kotlin/moodmanager/api/service/UserService.kt
@@ -1,37 +1,40 @@
 package moodmanager.api.service
 
 import moodmanager.api.model.User
+import moodmanager.api.modelDTO.Message
+import moodmanager.api.modelDTO.ResponseDTO
 import moodmanager.api.modelDTO.UserDTO
 import moodmanager.api.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
-class UserService(private val userRepository: UserRepository) {
+class UserService(private val userRepository: UserRepository, private val passwordEncoder: PasswordEncoder) {
 
     fun getAllUsers():ArrayList<UserDTO> {
-        return toMultipleDTO(this.userRepository.findAll() as ArrayList<User>)
+         val arrayUsers: ArrayList<UserDTO> = User.toMultipleDTO(this.userRepository.findAll() as ArrayList<User>)
+         return arrayUsers
     }
 
-    fun addUser(userDTO: UserDTO) {
-        userRepository.save(fromDTO(userDTO))
+    fun addUser(userDTO: UserDTO):  ResponseDTO {
+        if(usernameExist(userDTO.username)){
+            return ResponseDTO(null, Message("Usuário já existente.", "ENVALID-USERNAME"))
+        } else {
+            this.userRepository.save(this.fromDTO(userDTO))
+            return ResponseDTO(null, Message("Usuário cadastrado com sucesso!","SUCCESS"))
+        }
     }
 
+    fun register(userDTO: UserDTO): ResponseDTO {
+        userDTO.password = passwordEncoder.encode(userDTO.password)
+        return addUser(userDTO)
+    }
+
+    fun usernameExist( username: String) : Boolean {
+        return userRepository.existsByUsername(username)
+    }
 
      fun fromDTO(userDTO: UserDTO): User {
-         return User(0, userDTO.username)
+         return User(0, userDTO.username, userDTO.password, userDTO.geralScore, userDTO.)
      }
-
-     fun toDTO( obj: User): UserDTO {
-         return with(obj as User) {
-             UserDTO(obj)
-         }
-    }
-
-     fun toMultipleDTO(arr: ArrayList<User>): ArrayList<UserDTO> {
-        val userDTOS = ArrayList<UserDTO>()
-        arr.forEach {
-            userDTOS.add(this.toDTO(it))
-        }
-        return userDTOS
-    }
 }
