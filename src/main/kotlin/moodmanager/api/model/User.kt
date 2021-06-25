8f797f0b42be6d9bcf913f777621390d4ce21909
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-06-25 16:14:19 -0300

    Conclusion of User Repository Model

--- src/main/kotlin/moodmanager/api/model/User.kt
+++ src/main/kotlin/moodmanager/api/model/User.kt
@@ -1,5 +1,6 @@
 package moodmanager.api.model
 
+import moodmanager.api.modelDTO.UserDTO
 import javax.persistence.*
 
 @Entity
@@ -9,4 +10,36 @@ data class User (
     val id: Long = 0,
 
     val username: String = "",
+    val password: String = "",
+
+    val geralScore: Int = 0,
+
+
+    @OneToOne(mappedBy = "users")
+    val preference: Preference,
+
+    @ManyToMany(mappedBy = "users")
+    val registers: ArrayList<Register>,
+
+    @ManyToMany(mappedBy = "users")
+    val interests: ArrayList<Interest>
+
+
 )
+{
+    companion object {
+        fun toDTO( obj: User): UserDTO {
+            return with(obj as User) {
+                UserDTO(obj)
+            }
+        }
+
+        fun toMultipleDTO(arr: ArrayList<User>): ArrayList<UserDTO> {
+            val userDTOS = ArrayList<UserDTO>()
+            arr.forEach {
+                userDTOS.add(this.toDTO(it))
+            }
+            return userDTOS
+        }
+    }
+}
